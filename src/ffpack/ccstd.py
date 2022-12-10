cc8b6e7612862ce413b7df102f7747ecfa67a1a9
Dongping Zhu <zhudongping@hotmail.com>
2022-12-09 23:25:21 -0500

    Add the rainflowCounting function

---
+++ src/ffpack/ccstd.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python3
+
+'''
+This module implements the standard cycle counting methods in 
+ASTM E1049-85(2017) Standard Practices for Cycle Counting in Fatigue Analysis
+'''
+
+import numpy as np
+from collections import defaultdict, deque
+
+def levelCrossingCounting( data, levels=None ):
+    '''
+    Implement the level crossing counting method based on E1049-85: sec 5.1.1
+    By default, this method does the level crossing couting for each integers
+    Args:
+        data: 1D input sequence data for couning
+        leves: 1D input sequence levels
+
+    Returns:
+        rst: 2D sorted output data
+    '''
+    # Edge case check
+    data = np.array( data )
+    if len( data.shape ) != 1:
+        raise ValueError( "Input data dimension should be 1" )
+    if data.shape[0] <= 1:
+        raise ValueError( "Input data length should be at least 2")
+    if levels is None or len( levels )==0 :
+        minElement = np.floor( np.min( data ) )
+        maxElement = np.ceil( np.max( data ) )
+        numElement = maxElement - minElement + 1
+        levels =  np.linspace( minElement, maxElement, numElement.astype(int) )
+    else:
+        levels = np.array( sorted( set( levels ) ) )
+
+    rstDict = defaultdict( int )
+    # Check each interval
+    for i in range( len( data ) - 1 ):
+        intervalStart = data[ i ]
+        intervalEnd = data[ i + 1 ]
+        lowerVal = intervalStart if intervalStart <= intervalEnd else intervalEnd
+        upperVal = intervalEnd if intervalStart <= intervalEnd else intervalStart
+        leftIndex = np.searchsorted( levels, lowerVal, side='left' )
+        if i != 0 and leftIndex < len( levels ) and lowerVal == levels[ leftIndex ]:
+            leftIndex += 1
+        rightIndex = np.searchsorted( levels, upperVal, side='right' )
+        for j in range( leftIndex, rightIndex ):
+            if ( intervalStart <= intervalEnd and levels[ j ] >= 0 ) or \
+               ( intervalStart > intervalEnd and levels[ j ] < 0 ):
+                rstDict[ levels[ j ] ] += 1
+    if len( rstDict ) == 0:
+        return [ [] ]
+    rst = np.array( [ [ key, val ] for key, val in rstDict.items() ] )
+    rst = rst[ rst[ :, 0 ].argsort() ]
+    return rst.tolist()
+
+def peakCounting( data ):
+    pass
+
+def simpleRangeCounting( data ):
+    pass
+
+def rainflowCounting( data ):
+    '''
+    Implement the rainflow counting method based on E1049-85: sec 5.4.4
+    Args:
+        data: 1D input sequence data for couning
+        leves: 1D input sequence levels
+
+    Returns:
+        rst: 2D sorted output data
+    '''
+    # Edge case check
+    data = np.array( data )
+    if len( data.shape ) != 1:
+        raise ValueError( "Input data dimension should be 1" )
+    if data.shape[0] <= 1:
+        raise ValueError( "Input data length should be at least 2")
+
+    # Remove the intermediate value first
+    cleanData = []
+    for i, cur in enumerate( data ):
+        if i == 0 or i == len( data ) - 1:
+            cleanData.append( cur )
+        else:
+            prev = cleanData[ -1 ]
+            next = data[ i + 1 ]
+            if ( prev < cur and cur > next ) or \
+               ( prev > cur and cur < next ):
+               cleanData.append( cur )
+    data = np.array( cleanData )
+
+    if data.shape[0] <= 1:
+        return [ [] ] 
+
+    dequeA = deque()
+    dequeB = deque( [ i for i in data ] )
+    S = None
+    YContainsS = None
+    rstDict = defaultdict( int )
+    while len( dequeB ) >= 3:
+        A = dequeB.popleft()
+        B = dequeB.popleft()
+        C = dequeB.popleft()
+        Y = abs( A - B )
+        X = abs( B - C )
+        if S is None:
+            S = A
+            YContainsS = True
+        if X >= Y:
+            if YContainsS:
+                rstDict[ Y ] += 0.5
+                dequeB.appendleft( C )
+                dequeB.appendleft( B )
+                S = None
+                YContainsS = None
+            else:
+                rstDict[ Y ] += 1
+                dequeB.appendleft( C )
+                while dequeA:
+                    dequeB.appendleft( dequeA.pop() )
+                S = None
+                YContainsS = None
+        else:
+            YContainsS = False
+            dequeB.appendleft( C )
+            dequeB.appendleft( B )
+            dequeA.append( A )
+
+    while dequeA:
+        dequeB.appendleft( dequeA.pop() )
+    
+    A = dequeB.popleft()
+    while dequeB:
+        B = dequeB.popleft()
+        Y = abs( A - B )
+        rstDict[ Y ] += 0.5
+        A = B
+
+    if len( rstDict ) == 0:
+        return [ [] ] 
+    rst = np.array( [ [ key, val ] for key, val in rstDict.items() ] )
+    rst = rst[ rst[ :, 0 ].argsort() ]
+    return rst.tolist()
---
+++ tests/test_ccstd.py
@@ -0,0 +1,239 @@
+#!/usr/bin/env python3
+
+from ffpack import ccstd
+import numpy as np
+import pytest
+
+#
+# Test levelCorssingCounting function
+#
+def test_levelCrossingCounting_case1():
+    # Standard level corssing counting data from E1049-85(2017) Fig.2(a)
+    # No levels for this test case
+    data = [ -0.8, 1.3, 0.7, 3.4, 0.7, 2.5, -1.4, -0.5, -2.3, 
+             -2.2, -2.6, -2.4, -3.3, 1.5, 0.6, 3.4, -0.5 ]
+    calRst = ccstd.levelCrossingCounting( data )
+    expectedRst = [ [ -3.0, 1.0 ], [ -2.0, 1.0 ], [ -1.0, 2.0 ], [ 0.0, 2.0 ], 
+                    [ 1.0, 5.0 ], [ 2.0, 3.0 ], [ 3.0, 2.0 ] ] 
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    # Adding extra data into the standard data without change the slope
+    data = [ -0.8, 0.0, 1.0, 1.3, 0.7, 1.0, 2.0, 3.4, 3.0, 2.0, 
+             1.0, 0.7, 1.0, 2.0, 2.5, 2.0, 1.0, 0.0, -1.0, -1.4, 
+             -0.5, -1.0, -2.0, -2.3, -2.2, -2.6, -2.4, -3.0, -3.3, 
+             -2.0, -1.0, 0.0, 1.0, 1.5, 0.6, 1.0, 2.0, 3.0, 3.4, 
+             3.0, 2.0, 1.0, 0.0, -0.5 ]
+    calRst = ccstd.levelCrossingCounting( data )
+    expectedRst = [ [ -3.0, 1.0 ], [ -2.0, 1.0 ], [ -1.0, 2.0 ], [ 0.0, 2.0 ], 
+                    [ 1.0, 5.0 ], [ 2.0, 3.0 ], [ 3.0, 2.0 ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    # Slightly bias the data without change the slope
+    data = [ -0.8, 0.2, 1.1, 1.3, 0.7, 0.8, 2.3, 3.4, 3.2, 1.8, 
+             1.0, 0.7, 0.8, 2.1, 2.5, 1.8, 0.9, 0.0, -1.2, -1.4, 
+             -0.5, -0.9, -1.8, -2.3, -2.2, -2.6, -2.4, -3.1, -3.3, 
+             -2.2, -1.3, 0.1, 0.8, 1.5, 0.7, 1.3, 2.1, 2.8, 3.4, 
+             2.7, 2.1, 0.8, 0.1, -0.5 ]
+    calRst = ccstd.levelCrossingCounting( data )
+    expectedRst = [ [ -3.0, 1.0 ], [ -2.0, 1.0 ], [ -1.0, 2.0 ], [ 0.0, 2.0 ], 
+                    [ 1.0, 5.0 ], [ 2.0, 3.0 ], [ 3.0, 2.0 ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    # Trivial case 1
+    data = [ 0.0, 2.0 ]
+    calRst = ccstd.levelCrossingCounting( data )
+    expectedRst = [ [ 0.0, 1.0 ], [ 1.0, 1.0 ], [ 2.0, 1.0 ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    # Trivial case 2
+    data = [ -0.5, 2.5 ]
+    calRst = ccstd.levelCrossingCounting( data )
+    expectedRst = [ [ 0.0, 1.0 ], [ 1.0, 1.0 ], [ 2.0, 1.0 ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    # Trivial case 3
+    data = [ 2.0, 0.0 ]
+    calRst = ccstd.levelCrossingCounting( data )
+    expectedRst = [ [ ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    # Trivial case 4
+    data = [ 2.5, -0.5 ]
+    calRst = ccstd.levelCrossingCounting( data )
+    expectedRst = [ [ ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+    
+    # Trivial case 5
+    data = [ 2.5, -1.5 ]
+    calRst = ccstd.levelCrossingCounting( data )
+    expectedRst = [ [ -1.0, 1.0 ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    # Trivial case 6
+    data = [ 0.0, -2.5 ]
+    calRst = ccstd.levelCrossingCounting( data )
+    expectedRst = [ [ -2.0, 1.0 ] , [ -1.0, 1.0 ]]
+    np.testing.assert_allclose( calRst, expectedRst )
+    
+    # Trivial case 7
+    data = [ -2.5, -0.5 ]
+    calRst = ccstd.levelCrossingCounting( data )
+    expectedRst = [ [ ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+def test_levelCrossingCounting_case2():
+    # Test edge cases for empty list
+    data = [ ]
+    with pytest.raises( ValueError ):
+        _ = ccstd.levelCrossingCounting( data )
+
+    # Test edge cases for 1 element list
+    data = [ 1.0 ]
+    with pytest.raises( ValueError ):
+        _ = ccstd.levelCrossingCounting( data )
+
+    # Test edge cases for 2D list
+    data = [ [ 1.0 ], [ 2.0 ] ]
+    with pytest.raises( ValueError ):
+        _ = ccstd.levelCrossingCounting( data )
+
+def test_levelCrossingCounting_case3():
+    # Standard level corssing counting data from E1049-85(2017) Fig.2(a)
+    # Set the levels
+    data = [ -0.8, 1.3, 0.7, 3.4, 0.7, 2.5, -1.4, -0.5, -2.3, 
+             -2.2, -2.6, -2.4, -3.3, 1.5, 0.6, 3.4, -0.5 ]
+    levels = np.array( [ -3.0, -2.0, 2.0, 3.0 ] )
+    calRst = ccstd.levelCrossingCounting( data, levels=levels )
+    expectedRst = [ [ -3.0, 1.0 ], [ -2.0, 1.0 ], [ 2.0, 3.0 ], [ 3.0, 2.0 ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    # Add other levels
+    data = [ -0.8, 1.3, 0.7, 3.4, 0.7, 2.5, -1.4, -0.5, -2.3, 
+             -2.2, -2.6, -2.4, -3.3, 1.5, 0.6, 3.4, -0.5 ]
+    levels = [ -5, -4, -3, -2, 2, 3, 4, 5 ]
+    calRst = ccstd.levelCrossingCounting( data, levels=levels )
+    expectedRst = [ [ -3.0, 1.0 ], [ -2.0, 1.0 ], [ 2.0, 3.0 ], [ 3.0, 2.0 ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    # Trivial case 1
+    data = [ 0.0, 2.2 ]
+    levels = [ 0.0, 1.0 ]
+    calRst = ccstd.levelCrossingCounting( data, levels=levels )
+    expectedRst = [ [ 0.0, 1.0 ], [ 1.0, 1.0 ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    # Trivial case 2
+    data = [ 0.0, 2.2 ]
+    levels = [ 2.0 ]
+    calRst = ccstd.levelCrossingCounting( data, levels=levels )
+    expectedRst = [ [ 2.0, 1.0 ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    # Trivial case 3
+    data = [ 1.0, -2.5 ]
+    levels = [ -1.0 ]
+    calRst = ccstd.levelCrossingCounting( data, levels=levels )
+    expectedRst = [ [ -1.0, 1.0 ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+#
+# Test peakCounting function
+#
+
+#
+# Test simpleRangeCounting function
+#
+
+#
+# Test rainflowCounting function
+#
+def test_rainflowCounting_case1():
+    # Standard rainflow counting data from E1049-85(2017) Fig.6(a)
+    data = [ -2.0, 1.0, -3.0, 5.0, -1.0, 3.0, -4.0, 4.0, -2.0 ]
+    calRst = ccstd.rainflowCounting( data )
+    expectedRst = [ [ 3.0, 0.5 ], [ 4.0, 1.5 ], [ 6.0, 0.5 ], [ 8.0, 1.0 ], [ 9.0, 0.5 ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    # Adding extra data into the standard data without change the slope
+    data = [ -2.0, -0.5, 1.0, 0.0, -1.5, -3.0, -1.0, 2.5, 5.0, 3.0, 1.0, -0.5, -1.0, 2.0, 
+             3.0, -0.5, -3.5, -4.0, 2.0, 3.0, 4.0, 3.0, 1.0, -2.0 ]
+    calRst = ccstd.rainflowCounting( data )
+    expectedRst = [ [ 3.0, 0.5 ], [ 4.0, 1.5 ], [ 6.0, 0.5 ], [ 8.0, 1.0 ], [ 9.0, 0.5 ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    # Trivial case 1
+    data = [ 0.0, 1.5 ]
+    calRst = ccstd.rainflowCounting( data )
+    expectedRst = [ [ 1.5, 0.5 ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    # Trivial case 2
+    data = [ 1.5, 2.0 ]
+    calRst = ccstd.rainflowCounting( data )
+    expectedRst = [ [ 0.5, 0.5 ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    # Trivial case 3
+    data = [ 3.0, 1.0 ]
+    calRst = ccstd.rainflowCounting( data )
+    expectedRst = [ [ 2.0, 0.5 ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    # Trivial case 4
+    data = [ -0.5, -1.0 ]
+    calRst = ccstd.rainflowCounting( data )
+    expectedRst = [ [ 0.5, 0.5 ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    # Trivial case 5
+    data = [ 0.0, 1.5, 0.0 ]
+    calRst = ccstd.rainflowCounting( data )
+    expectedRst = [ [ 1.5, 1.0 ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    # Trivial case 6
+    data = [ -1.0, -2.5, -1.0 ]
+    calRst = ccstd.rainflowCounting( data )
+    expectedRst = [ [ 1.5, 1.0 ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    # Trivial case 7
+    data = [ 0.0, 1.5, 1.0 ]
+    calRst = ccstd.rainflowCounting( data )
+    expectedRst = [ [ 0.5, 0.5 ], [ 1.5, 0.5 ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    # Trivial case 8
+    data = [ 0.0, -2.5, -1.0 ]
+    calRst = ccstd.rainflowCounting( data )
+    expectedRst = [ [ 1.5, 0.5 ], [ 2.5, 0.5 ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    # Trivial case 9
+    data = [ 0.0, 2.5, 0.0, 3.0 ]
+    calRst = ccstd.rainflowCounting( data )
+    expectedRst = [ [ 2.5, 1.0 ], [ 3, 0.5 ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    # Trivial case 10
+    data = [ -1.0, 3.0, -0.5, 1.0, -2.0 ]
+    calRst = ccstd.rainflowCounting( data )
+    expectedRst = [ [ 1.5, 1.0 ], [ 4.0, 0.5 ], [ 5.0, 0.5 ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    # Trivial case 11
+    data = [ -1.0, 1.0, -3.0, 3.0, 2.0 ]
+    calRst = ccstd.rainflowCounting( data )
+    expectedRst = [ [ 1.0, 0.5 ], [ 2.0, 0.5 ], [ 4.0, 0.5 ], [ 6.0, 0.5 ] ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+def test_rainflowCounting_case2():
+    # Test edge cases for empty list
+    data = [ ]
+    with pytest.raises( ValueError ):
+        _ = ccstd.rainflowCounting( data )
+
+    # Test edge cases for 1 element list
+    data = [ 1.0 ]
+    with pytest.raises( ValueError ):
+        _ = ccstd.rainflowCounting( data )
