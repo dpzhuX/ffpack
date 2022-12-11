0bd94c856902260466ab6050b418e1c6c0e5de3e
Dongping Zhu <zhudongping@hotmail.com>
2022-12-10 21:02:33 -0500

    Update docs structure

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,11 +2,11 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'FFPack'
+copyright = '2022, Dongping Zhu'
+author = 'Dongping Zhu'
 
-release = '0.1'
+release = '0.1.0'
 version = '0.1.0'
 
 # -- General configuration
@@ -17,7 +17,10 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'nbsphinx',
 ]
+
+source_suffix = [".rst", ".md"]
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
