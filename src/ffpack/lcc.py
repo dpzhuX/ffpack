4e5275f43db0510b20dc19c935abb89173e99a3c
Dorothydingzx <zhixiading@outlook.com>
2022-12-13 11:07:14 -0500

    updated rychlikRainflowCycleCounting method and testing

--- src/ffpack/lcc.py
+++ src/ffpack/lcc.py
@@ -197,7 +197,7 @@ def astmRainflowCounting( data ):
     return rst.tolist()
 
 
-def rychlikRainflowCycleCounting( data ):
+def rychlikRainflowCycleCounting( data, aggragate=True ):
     '''
     Implement the rainflow counting method based on Definition 1
     in "A new definition of the rainflow cycle counting method" by Rychilk on IJF
@@ -209,33 +209,47 @@ def rychlikRainflowCycleCounting( data ):
     '''
 
     def getMinLeft( data, i ):
-        if (i == 1): return min(data[0], data[1])
+        if ( i == 1 ): 
+            return min( data[ 0 ], data[ 1 ] )
         
-        left = data[i - 1]
+        left = data[ i - 1 ]
         j = i - 2
-        while (j >= 0 and data[j] < data[i]): 
-            left = min(left, data[j])
+        while ( j >= 0 and data[ j ] < data[ i ] ): 
+            left = min( left, data[ j ] )
             j -= 1
 
         return left
 
     def getMinRight( data, i ):
-        if (i == len(data) - 2): 
-            return min(data[len(data) - 1], data[len(data) - 2])
+        if ( i == len( data ) - 2 ): 
+            return min( data[ len( data ) - 1 ], data[ len( data ) - 2 ] )
         
-        right = data[i + 1]
+        right = data[ i + 1 ]
         j = i + 2
-        while (j < len(data) and data[j] < data[i]):
-            right = min(right, data[j])
+        while ( j < len( data ) and data[ j ] < data[ i ] ):
+            right = min( right, data[ j ] )
             j += 1
 
         return right 
 
 
     peakVallays = utils.getSequencePeakAndValleys( data, keepEnds=True )
-    rstSeq = []
-    for i in range(1, len(data) - 1):
-        if (data[i] > data[i - 1] and data[i] > data[i + 1]):
-            rstSeq.append(min(data[i] - getMinLeft(data, i), data[i] - getMinRight(data, i)))
-            
-    return rstSeq
+    rstSeq = [ ]
+    for i in range( 1, len( data ) - 1 ):
+        if ( data[ i ] > data[ i - 1 ] and data[ i ] > data[ i + 1 ] ):
+            # TODO: set a global variable for round digit
+            height = data[ i ] - max( getMinLeft( data, i ), getMinRight( data, i ))
+            rstSeq.append( round( height, 7 ) )
+    
+    if ( not aggragate ): 
+        return rstSeq
+
+    rstDict = defaultdict( int )
+    for i, cur in enumerate( rstSeq ):
+        rstDict[ cur ] += 1
+
+    if len( rstDict ) == 0:
+        return [ [ ] ] 
+    rst = np.array( [ [ key, val ] for key, val in rstDict.items() ] )
+    rst = rst[ rst[ :, 0 ].argsort() ]
+    return rst.tolist()
