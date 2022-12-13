a60fe350a4d7192a943e80160c3dc888940095a8
Dongping Zhu <zhudongping@hotmail.com>
2022-12-13 11:29:01 -0500

    Add one test case

--- tests/test_lsg.py
+++ tests/test_lsg.py
@@ -4,12 +4,34 @@ from ffpack import lsg
 import numpy as np
 import pytest
 
-
 ###############################################################################
 # Test randomWalk
 ###############################################################################
-def test_randomWalk_normalUseCase_pass():
-    print(lsg.randomWalk( 5, 2 ))
+def test_randomWalk_normalUseCase_diffByOne():
+    calRst = lsg.randomWalk( 100, 1 )
+    sumRowRst = np.sum( np.array( calRst ), axis=1 ).astype( int )
+    for i in range( 1, len( sumRowRst ) ):
+        assert abs( sumRowRst[ i ] - sumRowRst[ i - 1 ] ) == 1
+
+    calRst = lsg.randomWalk( 100, 2 )
+    sumRowRst = np.sum( np.array( calRst ), axis=1 ).astype( int )
+    for i in range( 1, len( sumRowRst ) ):
+        assert abs( sumRowRst[ i ] - sumRowRst[ i - 1 ] ) == 1
+
+    calRst = lsg.randomWalk( 100, 3 )
+    sumRowRst = np.sum( np.array( calRst ), axis=1 ).astype( int )
+    for i in range( 1, len( sumRowRst ) ):
+        assert abs( sumRowRst[ i ] - sumRowRst[ i - 1 ] ) == 1
+
+    calRst = lsg.randomWalk( 100, 4 )
+    sumRowRst = np.sum( np.array( calRst ), axis=1 ).astype( int )
+    for i in range( 1, len( sumRowRst ) ):
+        assert abs( sumRowRst[ i ] - sumRowRst[ i - 1 ] ) == 1
+
+    calRst = lsg.randomWalk( 50, 8 )
+    sumRowRst = np.sum( np.array( calRst ), axis=1 ).astype( int )
+    for i in range( 1, len( sumRowRst ) ):
+        assert abs( sumRowRst[ i ] - sumRowRst[ i - 1 ] ) == 1
 
 def test_randomWalk_stepsLessThanOneCase_valueError():
     steps = 0
