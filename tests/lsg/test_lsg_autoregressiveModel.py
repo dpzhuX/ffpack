f127f6719fcec1e5e10ffc72df20760a1414e62e
Dorothydingzx <zhixiading@outlook.com>
2023-01-01 14:29:34 -0500

    moving-average model

--- tests/lsg/test_lsg_autoregressiveModel.py
+++ tests/lsg/test_lsg_autoregressiveModel.py
@@ -10,7 +10,7 @@ from unittest.mock import patch
 # Test arNormal
 ###############################################################################
 def test_arNormal_numStepsLessThanOneCase_valueError():
-    obs = [ 0, 0  ]
+    obs = [ 0, 0 ]
     phis = [ 0.5, 0.3 ]
     with pytest.raises( ValueError ):
         _ = lsg.arNormal( -1, obs, phis, 0, 0.5 )
@@ -20,7 +20,7 @@ def test_arNormal_numStepsLessThanOneCase_valueError():
 
 
 def test_arNormal_lengthsNotEqualCase_valueError():
-    obs = [ 0, 0  ]
+    obs = [ 0, 0 ]
     phis = [ 0.3 ]
     with pytest.raises( ValueError ):
         _ = lsg.arNormal( 500, obs, phis, 0, 0.5 )
@@ -61,6 +61,7 @@ def test_arNormal_ar2ThreeStepsCase_threePointsOutput( mock_get ):
     expectedRst = [ 1.0, 1.0, 2.0 ]
     np.testing.assert_allclose( calRst, expectedRst )
 
+
 @patch( "numpy.random.normal" )
 def test_arNormal_ar2FourStepsCase_threePointsOutput( mock_get ):
     obs = [ 0, 0 ]
@@ -96,4 +97,104 @@ def test_arNormal_ar2FourStepsCase_threePointsOutput( mock_get ):
     mock_get.return_value = [ 2.0, 1.0, -1.0, -2.0 ]
     calRst = lsg.arNormal( 4, obs, phis, 0, 0.5 )
     expectedRst = [ 1.0, 0.5, -1.0, -3.25 ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+
+
+###############################################################################
+# Test maNormal
+###############################################################################
+def test_maNormal_numStepsNotInt_valueError():
+    thetas = [ 0.5, 0.2 ]
+    with pytest.raises( ValueError ):
+        _ = lsg.maNormal( 1.2, 0, thetas, 0, 0.5 )
+
+
+def test_maNormal_numStepsLessThanOneCase_valueError():
+    thetas = [ 0.5, 0.2 ]
+    with pytest.raises( ValueError ):
+        _ = lsg.maNormal( -1, 0, thetas, 0, 0.5 )
+
+    with pytest.raises( ValueError ):
+        _ = lsg.maNormal( 0, 0, thetas, 0, 0.5 )
+
+
+def test_maNormal_thetasEmptyCase_valueError():
+    thetas = [  ]
+    with pytest.raises( ValueError ):
+        _ = lsg.maNormal( 500, 0, thetas, 0, 0.5 )
+
+
+@patch( "numpy.random.normal" )
+def test_maNormal_oneStepCase_outputNotRelatedToThetas( mock_get ):
+    mock_get.return_value = [ 1.0 ]
+
+    thetas = [ 0.5 ]
+    calRst = lsg.maNormal( 1, 0, thetas, 0, 0.5 )
+    expectedRst = [ 1.0 ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    thetas = [ 0.5, 0.2 ]
+    calRst = lsg.maNormal( 1, 0, thetas, 0, 0.5 )
+    expectedRst = [ 1.0 ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+
+@patch( "numpy.random.normal" )
+def test_maNormal_twoStepCase_outputRelatedToOneTheta( mock_get ):
+    thetas = [ 0.5 ]
+    mock_get.return_value = [ 1.0, 2.0 ]
+    calRst = lsg.maNormal( 2, 0, thetas, 0, 0.5 )
+    expectedRst = [ 1.0, 2.5 ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    thetas = [ 0.5, 0.2 ]
+    mock_get.return_value = [ 1.0, 2.0 ]
+    calRst = lsg.maNormal( 2, 0, thetas, 0, 0.5 )
+    expectedRst = [ 1.0, 2.5 ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+
+@patch( "numpy.random.normal" )
+def test_maNormal_threeStepCase_outputRelatedToTwoThetas( mock_get ):
+    mock_get.return_value = [ 1.0, 2.0, 3.0 ]
+
+    thetas = [ 0.5 ]
+    calRst = lsg.maNormal( 3, 0, thetas, 0, 0.5 )
+    expectedRst = [ 1.0, 2.5, 4.0 ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    thetas = [ 0.5, 0.2 ]
+    calRst = lsg.maNormal( 3, 0, thetas, 0, 0.5 )
+    expectedRst = [ 1.0, 2.5, 4.2 ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    thetas = [ 0.5, 0.2, 0.1 ]
+    calRst = lsg.maNormal( 3, 0, thetas, 0, 0.5 )
+    expectedRst = [ 1.0, 2.5, 4.2 ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+
+@patch( "numpy.random.normal" )
+def test_maNormal_fourStepCase_outputRelatedToThreeThetas( mock_get ):
+    mock_get.return_value = [ 1.0, 2.0, 3.0, 4.0 ]
+
+    thetas = [ 0.8 ]
+    calRst = lsg.maNormal( 4, 0, thetas, 0, 0.5 )
+    expectedRst = [ 1.0, 2.8, 4.6, 6.4 ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    thetas = [ 0.8, 0.5 ]
+    calRst = lsg.maNormal( 4, 0, thetas, 0, 0.5 )
+    expectedRst = [ 1.0, 2.8, 5.1, 7.4 ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    thetas = [ 0.8, 0.5, 0.2 ]
+    calRst = lsg.maNormal( 4, 0, thetas, 0, 0.5 )
+    expectedRst = [ 1.0, 2.8, 5.1, 7.6 ]
+    np.testing.assert_allclose( calRst, expectedRst )
+
+    thetas = [ 0.8, 0.5, 0.2, 0.1 ]
+    calRst = lsg.maNormal( 4, 0, thetas, 0, 0.5 )
+    expectedRst = [ 1.0, 2.8, 5.1, 7.6 ]
     np.testing.assert_allclose( calRst, expectedRst )
