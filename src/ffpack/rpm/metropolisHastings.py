189fe668301f36d2709795cfae7597c4be395215
Dongping Zhu <zhudongping@hotmail.com>
2023-01-15 12:37:55 -0500

    Update metropolisHastings.py

--- src/ffpack/rpm/metropolisHastings.py
+++ src/ffpack/rpm/metropolisHastings.py
@@ -6,23 +6,34 @@ class MetropolisHastingsSampler:
     '''
     Metropolis-Hastings sampler to sample data for arbitrary distribution.
     '''
-    def __init__( self, initialVal=None, targetPdf=None, proposalCSampler=None ):
+    def __init__( self, initialVal=None, targetPdf=None, proposalCSampler=None, 
+                  sampleDomain=lambda X: True ):
         '''
         Initialize the Metropolis-Hastings sampler
         
         Parameters
         ----------
-        initialVal: scalar or array_like of scalar
+        initialVal: scalar or array_like
             Initial observed data point.
         targetPdf: function
             Target probability density function or target distribution function.
             targetPdf takes one input parameter and return the corresponding 
-            probability.
+            probability. It will be called as targetPdf( X ) where X is the same 
+            type as initialVal, and a scalar value of probability should be returned.
         proposalCSampler: function
-            Proposal conditional sampler. proposalCSampler is a sampler that will 
-            return a sample for the given observed data point. A usual choice is to 
-            let proposalCSampler be a Gaussian/normal distribution centered at the 
-            observed data point.
+            Proposal conditional sampler (i.e., transition kernel). proposalCSampler 
+            is a sampler that will return a sample for the given observed data point. 
+            A usual choice is to let proposalCSampler be a Gaussian/normal 
+            distribution centered at the observed data point. It will be called as 
+            proposalCSampler( X ) where X is the same type as initialVal, and a 
+            sample with the same type of initialVal should be returned.
+        sampleDomain: function
+            Sample domain function. sampleDomain is a function to determine if a
+            sample is in the sample domain. For example, it the sample doamin is 
+            [ 0, inf ] and the sample is -2, the sample will be rejected. For the 
+            sampling on field of real numbers, it should return True regardless of 
+            the sample value. It called as sampleDomain( X ) where X is the same 
+            type as initivalVal, and a boolean value should be returned.
         
         Raises
         ------
@@ -49,6 +60,7 @@ class MetropolisHastingsSampler:
         self.nxt = initialVal
         self.targetPdf = targetPdf
         self.proposalCSampler = proposalCSampler
+        self.sampleDomain = sampleDomain
     
     def getAcceptanceRatio( self, candi ):
         fcur = self.targetPdf( self.cur )
@@ -76,7 +88,7 @@ class MetropolisHastingsSampler:
         candi = self.getCandidate()
         acceptanceRatio = self.getAcceptanceRatio( candi )
         u = np.random.uniform()
-        if u <= acceptanceRatio:
+        if u <= acceptanceRatio and self.sampleDomain( candi ):
             self.nxt = candi
         else:
             self.nxt = self.cur
